a573398c2cb74f2bfe11f12cd7d326704c0b287f
EsterLuiza25 <luiza.campossouza.25@gmail.com>
2025-09-23 15:43:07 -0300

    Adicionando arquivos

---
+++ transporte.py
@@ -0,0 +1,69 @@
+class VeiculoTransporte
+    def __init__ (self, placa, capacidadePassageiros):
+        self.placa = placa
+        self.__email = email
+
+
+    def Emitir_certificado(self):
+        return f"{self.__nome} emitindo um certificado de participação."
+
+
+class Aluno(participante) 
+     def __init__ (self, nome, email, curso):
+        super().__init__(nome, email)
+        self.__curso = curso
+
+    def Emitir_certificado(self):
+        return f"{self.__nome} emitindo um certificado de participação."
+
+
+class Instrutor(participante)
+     def __init__ (self,nome, email, especialidade):
+        super().__init__(nome, email)
+        self.__especialidade = especialidade
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main ():
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
